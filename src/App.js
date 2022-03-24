03ad9d152c6dc9306b7429a0683c93a658bd0317
kocierik <kocierik@gmail.com>
2022-03-24 17:01:09 +0100

    feat: convert to react

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [element, setElement] = useState(true)
+
+  const showElement = () => {
+    if (element) {
+      document.getElementById("block").style.visibility = "hidden"
+      setElement(false)
+    }
+    else {
+      document.getElementById("block").style.visibility = "visible"
+      setElement(true)
+    }
+  }
+
+  const createElement = () => {
+
+  }
+
+
+  return (
+    <div className="container">
+      <div className="title">Google</div>
+      <div className="search-bar">
+        <form method="get" action="https://www.google.com/search">
+          <input type="text" name="query" placeholder="grep '$PATH' google.com" />
+        </form>
+      </div>
+      <div className="element_block" id="block">  </div>
+      <div className="elements">
+        <div className="content"> <a href="https://web.telegram.org" target="_blank"> Telegram </a> </div>
+        <div className="content"> <a href="https://github.com" target="_blank"> Github </a> </div>
+        <div className="content"> <a href="https://web.whatsapp.com" target="_blank"> Whatsapp </a> </div>
+        <div className="content add" onClick={showElement}> + </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
