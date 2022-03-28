642d53eb77844d7d1e81f6e969db8b57740d19ce
kocierik <kocierik@gmail.com>
2022-03-28 17:50:38 +0200

    chore: remove padding hover

--- src/Box.jsx
+++ src/Box.jsx
@@ -6,16 +6,25 @@ function Box({ nameSite, link, id }) {
   const [favorite, setFavorite] = useContext(favoriteContext)
   let list = []
   list = favorite
+
   const deleteElement = () => {
     list = list.filter(item => item.id !== id)
     localStorage.removeItem("el" + id)
     setFavorite(list)
   }
 
+  const overOn = () => {
+    document.getElementById("checkHover").style.display = "block"
+  }
+  const overOf = () => {
+    document.getElementById("checkHover").style.display = "none"
+  }
+
+
   return (
-    <div className="content" > <a href={link} target="_blank">
+    <div className="content" onMouseOver={overOn} onMouseOut={overOf}> <a href={link} target="_blank">
       {nameSite} </a>
-      <div className="delete_button" onClick={deleteElement} >x</div>
+      <div className="delete_button" id="checkHover" onClick={deleteElement} >x</div>
     </div >
   );
 }
